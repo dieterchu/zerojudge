9cbd0ba375a291bb6fa00b8ad5f2c31dfac7a6b0
dieterchu <146905256+dieterchu@users.noreply.github.com>
2023-10-04 11:10:36 +0800

    20231014

---
+++ 基礎題庫/a003.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   int M,D,S;
+
+    cin>>M>>D;
+    S =(M*2+D)%3;
+    if(S == 0)
+       cout<<"普通\n";
+    else if(S == 1){
+       cout<<"吉\n";
+    }else{
+       cout<<"大吉\n";
+    }
+    return 0;
+}
